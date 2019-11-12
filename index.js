4cb55df926b2042d164437ea724ae8a740df8801
Oskar <oskaras34@gmail.com>
2019-11-12 13:43:19 +0200

    deleted useless comments from modals and some not used imports from index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const db = require('./db/db');
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const users = mongoose.model('users');
-const user = new users();
 const app = express();
 const jwt = require('jsonwebtoken');
 
--- models/project.model.js
+++ models/project.model.js
@@ -4,25 +4,21 @@ const projectSchema = new mongoose.Schema({
     
     projectName: {
         type: String,
-        // required: 'This field is required'
     },
     image: {
         type: String
     },
     projectLocation: {
         type: String,
-        // required: 'This field is required'
     },
     professionalsNeeded: {
         type: Array
     },
     projectArea: {
         type: String,
-        // required: 'This field is required'
     },
     projectShortDescription: {
         type: String,
-        // required: 'This field is required'
     },
     projectDescription: {
         type: String
