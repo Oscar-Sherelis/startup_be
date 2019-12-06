297126c639891a6092aeaefa9cd6d301fd8516f6
Oskar <oskaras34@gmail.com>
2019-12-06 20:18:25 +0200

    fixed req.params GET projects route

--- controllers/project/getProjects.js
+++ controllers/project/getProjects.js
@@ -7,15 +7,17 @@ const projectModel = mongoose.model('projects');
  * @param {*} next  Express next Function
  */
 
+//  made with pagination
  const getProjectsController = (req, res, next) => {
+   req.params.per = parseInt(req.params.per)
+   req.params.page = parseInt(req.params.page)
     projectModel.find({})
       .skip(req.params.per * req.params.page)
       .limit(req.params.per)
       .exec((err, projects) => {
-        console.log('projects: ' + projects)
         projectModel.countDocuments()
         .exec((err, count) => {
-          if(err) {
+          if (err) {
             return err
           }
           console.log(req.params.page)
